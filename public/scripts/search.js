d2c637e65767a7f4193746bc46625c3b46dfebd7
nicogusuarez <nicolas.gutierrezsuarez@student.kuleuven.be>
2023-05-10 10:34:42 +0200

    Added dummy data

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,1 +1,38 @@
-console.log("Hello")
+books = [{
+    "id": 1,
+    "isbn": 9780002005883,
+    "year": 2004,
+    "genre": "Thriller",
+    "title": "Gilead",
+    "subtitle": null,
+    "thumbnail": "http://books.google.com/books/content?id=KQZCPgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "average_rating": 3.85
+}, {
+    "id": 2,
+    "isbn": 9780002261982,
+    "year": 2000,
+    "genre": "Fantasy",
+    "title": "Spider's Web",
+    "subtitle": "A Novel",
+    "thumbnail": "http://books.google.com/books/content?id=gA5GPgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "average_rating": 3.83
+}, {
+    "id": 3,
+    "isbn": 9780006163831,
+    "year": 1982,
+    "genre": "Action & Adventure",
+    "title": "The One Tree",
+    "subtitle": null,
+    "thumbnail": "http://books.google.com/books/content?id=OmQawwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "average_rating": 3.97
+}, {
+    "id": 4,
+    "isbn": 9780006178736,
+    "year": 1993,
+    "genre": "Action & Adventure",
+    "title": "Rage of angels",
+    "subtitle": null,
+    "thumbnail": "http://books.google.com/books/content?id=FKo2TgANz74C&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "average_rating": 3.92
+}]
+
