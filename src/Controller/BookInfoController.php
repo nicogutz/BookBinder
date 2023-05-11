00333645cd3a6415c804432c46f8b3c8349427bc
nicogusuarez <nicolas.gutierrezsuarez@student.kuleuven.be>
2023-05-11 09:48:14 +0200

    Added dummy book data

--- src/Controller/BookInfoController.php
+++ src/Controller/BookInfoController.php
@@ -12,7 +12,20 @@ class BookInfoController extends AbstractController
     #[Route('/book_info', name: 'app_book_info')]
     public function terms(): Response
     {
-        return $this->render('book/book_info.html.twig');
+        $book = [
+            "id" => 2,
+            "year" => 2000,
+            "genre" => "action_adventure",
+            "title" => "Spiders Web",
+            "isbn13" => 9780002261982,
+            "subtitle" => "A Novel",
+            "thumbnail" => "http://books.google.com/books/content?id=gA5GPgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+            "description" => "A new Christie for Christmas  a fulllength novel adapted from her acclaimed play by Charles Osborne Following BLACK",
+        ];
+
+        return $this->render('book/book_info.html.twig', [
+            'book' => $book,
+        ]);
     }
 
 }
