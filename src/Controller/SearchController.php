ffc6cca8b00c83c3a01e662b011dae778388c453
nicogusuarez <nicolas.gutierrezsuarez@student.kuleuven.be>
2023-05-16 15:07:10 +0200

    Added serialization of Books for search API

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function Symfony\Component\String\u;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 
 class SearchController extends AbstractController
 {
@@ -15,4 +20,19 @@ class SearchController extends AbstractController
         return $this->render('search/search.html.twig', [
         ]);
     }
+
+    #[Route('/search_isbn/{isbn}', name: 'app_search_isbn', methods: ['GET'])]
+    public function searchISBN(string $isbn, BookRepository $repository): Response
+    {
+        $books = $repository->findByISBN($isbn);
+
+        return $this->json($books);
+    }
+    #[Route('/search_title/{title}', name: 'app_search_title', methods: ['GET'])]
+    public function searchTitle(string $title, BookRepository $repository): Response
+    {
+        $books = $repository->findByTitle($title);
+        return $this->json($books);
+    }
+
 }
