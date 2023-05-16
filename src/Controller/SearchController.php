f01baf0bc6960fce524f09018375b01f34c759a5
nicogusuarez <nicolas.gutierrezsuarez@student.kuleuven.be>
2023-05-16 15:10:58 +0200

    Removed unused imports

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -6,10 +6,6 @@ use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 
 class SearchController extends AbstractController
