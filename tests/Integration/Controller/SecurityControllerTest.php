7ced91cb8d306b5fb10b47b40c68b4f3cb0393f9
M.G <mg011201@outlook.com>
2023-05-25 19:58:25 +0200

    Modify SecurityControllerTest.php

--- tests/Integration/Controller/SecurityControllerTest.php
+++ tests/Integration/Controller/SecurityControllerTest.php
@@ -2,7 +2,6 @@
 
 namespace App\Tests\Integration\Controller;
 
-use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -42,9 +41,31 @@ class SecurityControllerTest extends WebTestCase
     {
         $this->client->request('GET', '/login');
         $test_user = $this->userRepo->findOneBy(['username' => 'test_user']);
-        $this->client->loginUser($test_user);
+        $crawler = $this->client->getCrawler();
+        $form = $crawler->filter('form[method="post"]')->form();
+        $form['username'] = 'test_user';
+        $form['password'] = 'password';
+        $crawler = $this->client->submit($form);
         $this->client->request('GET', '/');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('nav', 'test_user | Logout');
     }
+
+    /**
+     * @depends testSubmitValidData
+     */
+    public function testSubmitInvalidUsername():void
+    {
+        $this->client->request('GET', '/login');
+        $test_user = $this->userRepo->findOneBy(['username' => 'test_user']);
+        $crawler = $this->client->getCrawler();
+        $form = $crawler->filter('form[method="post"]')->form();
+        $form['username'] = 'wrong_user';
+        $form['password'] = 'password';
+        $crawler = $this->client->submit($form);
+        $this->client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('nav', 'Login');
+    }
+
 }
