1cbe5f424490b54adddad0dda30c71ce24348848
M.G <mg011201@outlook.com>
2023-05-28 16:25:14 +0200

    Added test for UserRepository

---
+++ tests/Integration/Repository/UserRepositoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Tests;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Entity\User;
+
+class UserRepositoryTest extends KernelTestCase
+{
+    private $entityManager;
+    private UserRepository|null $repository;
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+        $this->assertSame('test', $kernel->getEnvironment());
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+        $this->repository = $this->entityManager->getRepository(User::class);
+    }
+
+    /**
+     * @covers \App\Repository\UserRepository::save
+     */
+    public function testSaveUser(): void
+    {
+        $user = new User();
+        $user->setUsername('testuser')
+            ->setPassword('password');
+        $this->repository->save($user);
+        $this->entityManager->flush();
+        $savedUser = $this->repository->findOneBy(['username' => $user->getUsername()]);
+        $this->assertEquals(
+            $user->getUsername(),
+            $savedUser->getUsername(),
+            "The saved username should be identical to the actual one."
+        );
+    }
+
+    /**
+     * @covers \App\Repository\UserRepository::remove
+     * @depends testSaveUser
+     */
+    public function testRemoveUser() {
+        $user = new User();
+        $user->setUsername('testuser')
+            ->setPassword('password');
+        $this->repository->save($user);
+        $this->entityManager->flush();
+        $this->repository->remove($user);
+        $this->entityManager->flush();
+        $removedUser = $this->repository->findOneBy(['username' => $user->getUsername()]);
+        $this->assertNull($removedUser,"The removed user should not exist anymore.");
+    }
+
+    protected function tearDown(): void {
+        parent::tearDown();
+        $this->entityManager->close();
+        $this->entityManager = null;
+        $this->entityRepository = null;
+    }
+}
