eaa24de41892cc038c3c93a12bc5e82b7e693fe7
nicogusuarez <nicolas.gutierrezsuarez@student.kuleuven.be>
2023-05-30 12:14:35 +0200

    Add author to Books in admin

--- src/Controller/Admin/BookCrudController.php
+++ src/Controller/Admin/BookCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Book;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 
 class BookCrudController extends AbstractCrudController
 {
@@ -12,14 +16,22 @@ class BookCrudController extends AbstractCrudController
         return Book::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')
+            ->onlyOnIndex();
+        yield Field::new('ISBN13');
+        yield Field::new('title');
+        yield Field::new('subtitle');
+        yield Field::new('thumbnail');
+        yield Field::new('description')->hideOnDetail();
+        yield Field::new('year');
+        yield Field::new('pageNumber');
+        yield Field::new('averageRating');
+        yield Field::new('ratingCount');
+        yield Field::new('price');
+        yield Field::new('genre');
+        yield AssociationField::new('authors')
+            ->autocomplete();
     }
-    */
 }
--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -69,4 +69,9 @@ class Author
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
 }
